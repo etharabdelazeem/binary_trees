288b63fbd4b00050230d67825a2540ee3c832b94
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-03-26 00:27:15 -0700

    task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - function that finds if a node is sibling
+ * @node: node to check
+ * Return : sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	else
+	{
+	return(node->parent->left);
+	}
+}
