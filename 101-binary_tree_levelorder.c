1f633586a8e2d2d581c6290e2be6eaacf0741abe
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-03-26 12:50:56 +0000

    betty fix task 101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,7 +2,7 @@
 #include "5-binary_tree_is_root.c"
 
 /**
- * binary_tree_preorder - goes through a binary tree using level-order traversal.
+ * binary_tree_levelorder - goes through the tree using level-order traversal.
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  *
--- 101-main.c
+++ 101-main.c
@@ -24,12 +24,13 @@ int main(void)
 	        root = binary_tree_node(NULL, 98);
 		    root->left = binary_tree_node(root, 12);
 		        root->right = binary_tree_node(root, 402);
-			    root->left->left = binary_tree_node(root->left, 6);
-			        root->left->right = binary_tree_node(root->left, 56);
-				    root->right->left = binary_tree_node(root->right, 256);
-				        root->right->right = binary_tree_node(root->right, 512);
-
-					    binary_tree_print(root);
+			    root->left->right = binary_tree_node(root->left, 54);
+			        root->right->right = binary_tree_node(root->right, 128);
+				    root->left->left = binary_tree_node(root->left, 10);
+				        root->right->left = binary_tree_node(root->right, 45);
+					    root->right->right->left = binary_tree_node(root->right->right, 92);
+					        root->right->right->right = binary_tree_node(root->right->right, 65);
+						    binary_tree_print(root);
 					        binary_tree_levelorder(root, &print_num);
 						    binary_tree_delete(root);
 						        return (0);
