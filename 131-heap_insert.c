0cae55aa96200336314158ccb2c02774f9893af8
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-03-27 16:28:34 -0700

    task 36,37,38,39,40

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Function that measures the height of a binary tree
- * @tree: tree to go through
- * Return: the height
+ * heap_insert - inserts a value in Max Binary Heap
+ * @root: double pointer to root of tree
+ * @value: input value
+ * Return: pointer to the created node, or NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
 {}
