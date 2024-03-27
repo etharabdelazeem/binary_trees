2a9eb72a3663027314b483e9fe02dc2cfe407e5c
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-03-27 14:11:26 -0700

    task 30,31,32,33,34

--- binary_trees.h
+++ binary_trees.h
@@ -57,5 +57,10 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 
 #endif
