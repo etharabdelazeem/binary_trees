95acdcd4c493f9a41fc149c2188fe87adff10476
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-03-27 01:26:19 -0700

    task 23

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,25 +1,23 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_rotate_right - rotate the tree to right 
+ * binary_tree_rotate_right - rotate the tree to right
  * @tree: root node
  * Return: the new parent
  */
-binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)    
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-        binary_tree_t *parent;
+	binary_tree_t *parent;
 
-        if (!tree || !tree->left)
-                return (tree);
-        parent = tree->left;
-        tree->left = parent->right;
+	if (tree != NULL || tree->left != NULL)
+		return (tree);
+	parent = tree->left;
+	tree->left = parent->right;
 
-        if (tree->left != NULL)
-                tree->left->parent = tree;
+	if (tree->left != NULL)
+		tree->left->parent = tree;
+	parent->right = tree;
+	parent->parent = tree->parent;
+	tree->parent = parent;
 
-        parent->right = tree;
-        parent->parent = tree->parent;
-        tree->parent = parent;
-
-        return (parent);
+	return (parent);
 }
